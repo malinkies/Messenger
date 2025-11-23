e87f96f9cfc663994abf7615e928850543324b36
elvina <elvina18032007@gmail.com>
2025-11-23 21:49:48 +0300

    change user

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
+/*
+Стейси
+Джейкоб
+Боб
+Аманда
+Нил
+ */
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Создайте пять различных пользователей: ");
+        User[] masUsers = new User[5];
+        for (int i = 0; i < 5; i++) {
+            User user = new User(scanner.nextLine());
+            masUsers[i] = user;
+        }
+        System.out.println("Выберете ваш аккаунт:");
+        for (int i = 0; i < 5; i++) {
+            masUsers[i].creatUser();
+        }
+        User admin = new User(scanner.nextLine());
+        admin.menu();
+        admin.getUsers(masUsers, admin); //крч возвращает всех ост пользователей, которые не явл админом
+        //String action = scanner.nextLine(); //тип ввод операции, которую хочет совершить админ
 
     }
 }
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,2 +1,45 @@
+import java.util.Random;
+
 public class User {
+    protected String userName;
+
+    public User(String userName) {
+        this.userName = userName;
+    }
+
+    public User() {
+    }
+
+    public void creatUser() {
+        Random random = new Random();
+        String[] colors = {
+                "\u001B[38;2;135;206;235m",
+                "\u001B[38;2;238;130;238m",
+                "\u001B[38;2;100;149;237m",
+                "\u001B[38;2;220;20;60m",
+                "\u001B[38;2;255;165;0m",
+                "\u001B[38;2;0;250;154m",
+                "\u001B[38;2;255;105;180m",
+                "\u001B[38;2;0;255;0m",
+                "\u001B[38;2;0;191;255m",
+                "\u001B[38;2;255;255;0m",
+
+        };
+        int color = random.nextInt(colors.length);
+        System.out.println(colors[color] + userName + "\u001B[0m");
+    }
+    public void menu() {
+        System.out.println("""
+                Вам доступны следующие операции:\s
+                \u25CF Написать сообщение\s
+                \u25CF Зайти на канал\s
+                \u25CF Прочитать сообщения""");
+    }
+    public User[] getUsers(User[] masUsers, User admin) {
+        User[] users = new User[5];
+        for (int i = 0; i < 5; i++ ) {
+            if (!admin.equals(masUsers[i])) users[i] = masUsers[i];
+        }
+        return users;
+    }
 }
