f12f9fbd1fc560f4dfc8c14936ca10d84cc7a52c
Malika <malinonim@gmail.com>
2025-11-23 21:12:16 +0300

    сделан класс Message

--- src/main/java/Message.java
+++ src/main/java/Message.java
@@ -1,4 +1,21 @@
 public class Message {
     protected String text;
-    String date;
+    protected String date;
+    protected User sender;
+    protected User receiver;
+
+    public Message(String text, String date, User sender, User receiver) {
+        this.text = text;
+        this.date = date;
+        this.sender = sender;
+        this.receiver = receiver;
+    }
+
+    public String toString(){
+        return "от" + this.sender + "к" + this.receiver + ":\n" + this.text;
+    }
+
+    public String getText() {
+        return text;
+    }
 }
