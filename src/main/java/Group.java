ba7199ff59d38d55c250bed90bb8667db4a058e2
Malika <malinonim@gmail.com>
2025-11-23 22:03:04 +0300

    сделан класс Message

--- src/main/java/Group.java
+++ src/main/java/Group.java
@@ -1,4 +1,14 @@
 public class Group {
     protected String name;
     protected Message[] messages;
+
+    public Group(String name, Message[] messages) {
+        this.messages = messages;
+        this.name = name;
+    }
+    public void printMessages(){
+        for (Message msg : messages) {
+            System.out.println("");
+        }
+    }
 }
