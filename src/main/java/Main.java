ff3ef7b18a65036168c5bfd9cd70fbc5747a8c37
Malika <malinonim@gmail.com>
2025-11-24 19:46:10 +0300

    Обнова в Message обновлен Group

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
+/*
+Стейси
+Джейкоб
+Боб
+Аманда
+Нил
+ */
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Создайте пять различных пользователей: ");
+        User[] masUsers = new User[5];
+        for (int i = 0; i < 5; i++) {
+            User user = new User(scanner.nextLine());
+            masUsers[i] = user;
+        }
+        System.out.println("Выберете ваш аккаунт:");
+        for (int i = 0; i < 5; i++) {
+            masUsers[i].creatUser();
+        }
+        User admin = new User(scanner.nextLine());
+        admin.menu();
+        admin.getUsers(masUsers, admin); //крч возвращает всех ост пользователей, которые не явл админом
+        //String action = scanner.nextLine(); //тип ввод операции, которую хочет совершить админ
 
+        Message message = new Message("приветики", "вчера", masUsers[0]);
+        Group group1 = new Group("УРА", new Message[]{message});
+        group1.sendMessage("прив", "сегодня", masUsers[1]);
+        group1.printMessages();
     }
 }
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,2 +1,54 @@
+import java.util.Random;
+
 public class User {
+    protected String userName;
+    protected String colorCode;
+
+    public User(String userName) {
+        this.userName = userName;
+        this.colorCode = generateColor();
+    }
+
+    public User() {
+    }
+    public String getUserName() {
+        return this.userName;
+    }
+    public String generateColor() {
+        Random random = new Random();
+        String[] colors = {
+                "\u001B[38;2;135;206;235m",
+                "\u001B[38;2;238;130;238m",
+                "\u001B[38;2;100;149;237m",
+                "\u001B[38;2;220;20;60m",
+                "\u001B[38;2;255;165;0m",
+                "\u001B[38;2;0;250;154m",
+                "\u001B[38;2;255;105;180m",
+                "\u001B[38;2;0;255;0m",
+                "\u001B[38;2;0;191;255m",
+                "\u001B[38;2;255;255;0m",
+
+        };
+        int color = random.nextInt(colors.length);
+        return colors[color];
+    }
+    public static User[] getUsers(User[] masUsers, User admin) {
+        User[] users = new User[4];
+        int index = 0;
+        for (int i = 0; i < 5; i++) {
+            if (!masUsers[i].userName.equals(admin.userName)) users[index++] = masUsers[i];
+        }
+        return users;
+    }
+    public String codeAdmin(User[] masUsers, User admin) {
+        for (int i = 0; i < 5; i++) {
+            if (masUsers[i].userName.equals(admin.userName)) return masUsers[i].colorCode;
+        }
+        return null;
+    }
+
+    public String toString() {
+        return colorCode + userName + "\u001B[0m";
+    }
+
 }
